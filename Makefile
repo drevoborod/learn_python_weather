f10906893a0a270c73214d12cfade0cc29e4daa5
drevoborod <drevoborod@gmail.com>
2025-01-06 12:31:40 +0300

    some tests added

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 lint:
 	mypy weather
 
+test:
+	pytest tests
+
 run:
 	uvicorn weather.main:app --host 0.0.0.0 --port 8881 --log-level debug
---
+++ tests/data/weather_api_client.py
@@ -0,0 +1,10 @@
+GET_EXISTING_CITY_RESPONSE_BODY = [
+    {
+        "name": "Atlantis",
+        "country": "USA",
+        "lat": 123.1,
+        "lon": 321.4
+    }
+]
+
+GET_NON_EXISTING_CITY_RESPONSE_BODY = []
