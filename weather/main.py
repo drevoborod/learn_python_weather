0d9b45c1b49c1e4cee22516bf4764624db83cf47
drevoborod <drevoborod@gmail.com>
2025-01-27 21:55:59 +0300

    unnecessary root endpoint declaration removed

--- weather/main.py
+++ weather/main.py
@@ -1,12 +1,7 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 
 from weather.routers import router
 
 
 app = FastAPI(title="City temperature retrieval service", docs_url="/")
 app.include_router(router)
-
-
-@app.get("/", summary="Service root and docs URL")
-async def root(request: Request):
-    return
